6a44efee98e6cac5d4d5d5f88653606a8d7d7da6
AkiLetschne <akiletschne@gmail.com>
2023-11-06 20:22:50 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+
+setup(
+    name='restocks_api_wrapper',
+    version='0.1.0',
+    packages=find_packages(exclude=("tests",)),
+    url='https://github.com/AkiLetschne/restocks_api_wrapper',
+    license='MIT',
+    author='AkiLetschne',
+    description='A unofficial Python wrapper for the Restocks API',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires=["requests", "lxml"],
+    python_requires='>=3.11',
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
